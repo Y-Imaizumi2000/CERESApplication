62e0cc1b6d415d54fece92c6b623ce9bdfbab716
YuImaizumi2000 <yuimaizumi2000@gmail.com>
2026-03-15 18:20:33 +0900

    ヘッダーアイコン押下時の遷移先を変更

---
+++ src/main/resources/static/js/app/CAAF0001.js
@@ -0,0 +1,97 @@
+/**
+ * CAAF0001 クライアント処理
+ */
+
+// ---------------------------------------------
+// イベント登録
+// ---------------------------------------------
+document.getElementById("regInquiryInfo").addEventListener("submit", handleSubmit);
+
+
+// ---------------------------------------------
+// メイン処理
+// ---------------------------------------------
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const name   = document.querySelector('input[inquiryType="inquiryType"]').value.trim();
+    const pass = document.querySelector('input[message="message"]').value.trim();
+    const errors = [];
+
+    clearErrorArea();
+
+    // --- クライアント側バリデーション ---
+    checkValid(name, pass, confirmPass, errors);
+
+    if (errors.length > 0) {
+        displayErrors(errors);
+        return;
+    }
+
+    // --- ユーザー情報の登録） ---
+    regUserInfo(name, dob, telNo, pass);
+}
+
+
+// ---------------------------------------------
+// バリデーション
+// ---------------------------------------------
+function checkValid(name, pass, confirmPass, errors) {
+    if (pass !== confirmPass) {
+        errors.push("パスワードと確認用パスワードが一致しません。");
+    }
+}
+
+
+// ---------------------------------------------
+// サーバー通信
+// ---------------------------------------------
+function regUserInfo(name, dob, telNo, pass) {
+	ApiClient.post("/regUserInfo", {
+        name,
+        dob,
+        telNo,
+        pass
+    })
+    .then(result => {
+
+        if (!result.success) {
+            displayErrors(result.errors);
+            return;
+        }
+
+        console.log("登録成功");
+        alert("登録が完了しました。お客さまのユーザーIDは「" + result.userId + "」です。");
+		location.href = "/html/app/CAAA0001.html";
+
+    })
+    .catch(err => {
+        console.error("通信エラー:", err);
+    });
+}
+
+
+// ---------------------------------------------
+// エラー表示
+// ---------------------------------------------
+function clearErrorArea() {
+    document.getElementById("errorMessage").innerHTML = "";
+}
+
+function displayErrors(errors) {
+	if (!errors) return;
+    const errorArea = document.getElementById("errorMessage");
+    const ul = document.createElement("ul");
+
+    ul.style.color = "red";
+    ul.style.margin = "0";
+    ul.style.padding = "0 0 0 20px";
+
+    errors.forEach(msg => {
+        const li = document.createElement("li");
+        li.textContent = msg;
+        ul.appendChild(li);
+    });
+
+    errorArea.appendChild(ul);
+}
